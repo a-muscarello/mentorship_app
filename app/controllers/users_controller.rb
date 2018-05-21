a496aa42c2ce95190a2232a8648941660ae74380
Ambika Muscarello <ammuscarello@gmail.com>
2018-05-21 11:47:22 -0400

    add a show route to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
 
     end
 
+    def show
+        @user = User.find(params[:id])
+   
+    end
+
+    # private
+
+    # def user_params
+    #     params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+    # end
+    
 end
