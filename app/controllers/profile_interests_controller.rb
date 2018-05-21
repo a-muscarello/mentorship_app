493f89532c09a1b18ec228dc78f34ef71ea49c5c
Ambika Muscarello <ammuscarello@gmail.com>
2018-05-21 18:56:56 -0400

    made change to profile interest controller

--- app/controllers/profile_interests_controller.rb
+++ app/controllers/profile_interests_controller.rb
@@ -4,33 +4,33 @@ class ProfileInterestsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
-        @user = User
+        @user = ProfileInterest
     end
 
     def show
-        @user = User.find(user_params)
+        @user = ProfileInterest.where(user_id :current_user.id)
     end
 
     def edit
-        @user = User.find(params[:id])
+        @user = ProfileInterest.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
+        @user = ProfileInterest.find(params[:id])
         @user.update(user_params)
-        redirect_to '/users/profiles'
+        redirect_to '/show'
     end
 
     def destroy
-        @user = User.find(params[:id])
+        @user = ProfileInterest.find(params[:id])
         @user.delete
-        redirect_to '/layout/application'
+        redirect_to '/home'
     end
 
     private
 
     def find_user
-        @user = User.find(params[:id])
+        @user = ProfileInterest.find(params[:id])
     end
 
     def user_params
@@ -40,7 +40,5 @@ class ProfileInterestsController < ApplicationController
     # def user_params
     #     params.require(:user).permit(:first_name, :last_name, :username, :email, :encrypted_password, :avatar, :about, :mentor_id) / or :mentee_id
     # end
-
-end
 
 end
