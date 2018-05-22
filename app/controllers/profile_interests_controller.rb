d2ab939efc2996738d7b31b6f6d264c671c0e712
FRANCIS AGBODJI <frank@digitalfrank.com>
2018-05-22 11:10:05 -0400

    registration + interests => profile

---
+++ app/controllers/profile_interests_controller.rb
@@ -0,0 +1,48 @@
+
+class ProfileInterestsController < ApplicationController
+    
+    def index
+        @interests = ProfileInterest.create(:user_id => current_user.id)
+    end
+
+    def show
+        @interests = ProfileInterest.find(params[:id])
+    end
+    
+    def new
+        @interests = ProfileInterest.new
+    end
+    
+    def create
+        @interests = ProfileInterest.new(interests_params)
+        @interests.save
+        # redirect_to "/profile_interests/#{@interests.id}/"
+        # redirect_to "/users/profile"
+    end
+    
+    def edit
+        @interests = ProfileInterest.find(params[:id])
+    end
+    
+    def update
+        @interests = ProfileInterest.find(params[:id])
+        @interests.update(interests_params)
+        redirect_to "/users/profile"
+    end
+    
+    def destroy
+        @interests = ProfileInterest.find(params[:id])
+        @interests.destroy
+        redirect_to "/profile_interests"
+    end
+
+
+
+
+    private 
+
+    def interests_params 
+        params.require(:profile_interest).permit(:user_id, :python, :rails, :react, :node_js, :sql, :blockchain, :data_scraping, :javascript, :java, :scss, :mentee, :mentor)
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  has_many :chat_rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :connections, dependent: :destroy
+
+  has_one :profile_interest
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+       :recoverable, :rememberable, :trackable, :validatable
 
-  has_one :profile_interest
+
+  accepts_nested_attributes_for :profile_interest
 end
