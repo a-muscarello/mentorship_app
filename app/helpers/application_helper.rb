02d77f9cad9b0da578e453a3d4b20062a203003b
Ambika Muscarello <ammuscarello@gmail.com>
2018-05-24 17:29:50 -0400

    add modal for viewing other profile, add buttons for rating mentors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
     def can_see?(private_chat_room)
         current_user.id == private_chat_room.user_id || current_user.id == private_chat_room.other_user_id
     end
+
+    def can_score?(other_user_id)
+        mentored1 = PrivateChatRoom.find_by(user_id: current_user.id, other_user_id: other_user_id)
+        mentored2 = PrivateChatRoom.find_by(user_id: other_user_id, other_user_id: current_user.id)
+        puts mentored1
+        puts mentored2
+        if mentored1 != nil || mentored2 != nil
+            return true
+        end
+        return false
+    end
 end
+
