e38ff2e71badb496869d4e44de6a950bcc013029
Danielle Danskin <danielle.danskin@gmail.com>
2018-05-24 22:07:51 -0400

    fix other profile view to show correct user profile in modal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
     skip_before_action :authenticate_user!, :only => [:home, :show]
-
+    before_action :set_other_user, only: [:show]
     def home
-       
+
     end
 
     def show
-        @other_user = User.find_by(params[:id])
+        puts "i'm in the in show"
+        @other_user = User.find(params[:id])
     end
 
     def profile
         @user = current_user
         @interests = ProfileInterest.find_by(:user_id => current_user.id)
         @users = User.all
-        if @interests.mentor == true 
+        if @interests.mentor == true
             @results = ProfileInterest.where(:mentee => true)
         else
             @results = ProfileInterest.where(:mentor => true)
@@ -22,7 +23,9 @@ class UsersController < ApplicationController
 
 
     private
-
+    def set_other_user
+        @other_user = User.find_by(params[:user_id])
+    end
     def user_params
         params.require(:user).permit(
           :first_name, :last_name, :username, :email, :password
