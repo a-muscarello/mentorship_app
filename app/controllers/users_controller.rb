b33254f950688cb2378d7df2c30e6cd02c2961bb
Danielle Danskin <danielle.danskin@gmail.com>
2018-05-24 22:16:23 -0400

    remove test code snippets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,10 @@
 class UsersController < ApplicationController
     skip_before_action :authenticate_user!, :only => [:home, :show]
-    before_action :set_other_user, only: [:show]
     def home
 
     end
 
     def show
-        puts "i'm in the in show"
         @other_user = User.find(params[:id])
     end
 
@@ -23,9 +21,6 @@ class UsersController < ApplicationController
 
 
     private
-    def set_other_user
-        @other_user = User.find_by(params[:user_id])
-    end
     def user_params
         params.require(:user).permit(
           :first_name, :last_name, :username, :email, :password
